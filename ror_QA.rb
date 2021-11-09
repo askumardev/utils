9ba106929726f969cd372f8a2850105c0eb78943
sk534 <sk534@hotmail.com>
2021-11-09 18:45:27 +0530

    dir structure added

--- ror_QA.rb
+++ ror_QA.rb
@@ -3,8 +3,31 @@
 2) What is the current version of Rails you are using?
 --> 6.0.1.4
 3) Advantages of using Ruby or Ruby On Rails compared to other languages and frameworks?
+-->
 
 4) Explain the directory structure of Rails framework?
+--> File/Folder			Description
+	app		It works as the remainder of this directory. Basically it organizes our application component. It holds MVC.
+	app/assets	This folder contains static files required for application's front-end grouped into folders based on their type.
+	app/controllers	All the controller files are stored here. A controller handles all the web requests from the user.
+	app/helpers	It contains all the helper functions to assist MVC.
+	app/mailers	It contains mail specific functions for the application.
+	app/models	It contains the models and data stored in our application's database.
+	app/views	This folder contains the display templates to fill data in our application.
+	bin		It basically contains Rails script that start your app. It can also contain other scripts use to setup, upgrade or run 				the app.
+	config		It configures our application's database, routes and more.
+	db		It contains our current database schema and database migrations.
+	lib		It contains extended module for your application.
+	log		It contains application log files.
+	public		It contains static files and compiled assets. This is the only folder seen by the world.
+	test		It contains unit tests, other test apparatus and fixtures.
+	tmp		It contains temporary files like cache and pid files.
+	vendor		It contains all third-party code like vendor gems.
+	Gemfile		Here all your app's gem dependencies are declared. It is mandatory as it includes Rails core gems among other gems.
+	Gemfile.lock	It holds gems dependency tree including all versions for the app.
+	README.md	It is a brief instruction manual for your application.
+	Rakefile	It locates and loads tasks that can be run from the command line.
+	config.ru	Rack configuration for Rack based servers used to start the application.
 5) Explain MVC architecture ?
 6) Where do you configure the database for your Rails app?
 7) Where do u configure the basic configuration for your app?
@@ -98,6 +121,7 @@
 25.how do you validate uniqueness of a record by two column ?
 26. Explain unAbstrusive javascript in rails ?
 27. what is 1/0
+--> ZeroDivisionError (divided by 0)
 28. how do you do exception handling in ruby & how do you create custom exceptions in ruby ?
 29. how do you submit forms remotely in rails?
 30. what is duck typing in ruby with example ?
