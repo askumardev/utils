df216265f00a84d2e0ccb847bc4b12f575e3b4cd
askumardev <askumar.dev@outlook.com>
2022-03-02 13:16:14 +0530

    interview questions

---
+++ my_interview_stuff/new_interview_QnA.rb
@@ -0,0 +1,74 @@
+1)diff btn has many through and habtm? also which is better?
+2)what are validations in rails? at what moment of objects life cycle validations are checked?
+3)what do u mean by polymorphic association?when can one use this association?
+4)what are scopes in rails?
+5)difference btn class methods and scopes? are they similar?
+6)what are active record callbacks. explain some of them
+7)how can u define custom validation in rails
+8)what is STI? what are advantages and disadvntages.
+9)what are migrations in rails?what is the usage of rails migratins
+10)what do u mean by N+1 query? how can we resolve N+1 query with example.
+==========================================================
+1)difference between includes adn joins
+2)what do u mean by self_join association in rails
+3)what do u mean by ORM? explain
+4)is it possible to change the convention of naming a table in rails. 
+  if yes, explain how will you do this.
+5)what is rollong back a migration. how to rollback a specific migration
+6)what are relational and conditional callbacks in ActiveRecord and explain.
+7)what are transactional callbacks and explain
+8)what are member and collection routes and explain
+9)what do u knpw about namespace and scoped routing. what are the differences between them
+10)explain difference between webpacker and sprockets?
+=======================================================
+1)how many associations in rails?
+2)what do u mean by partials in rails? explain usage of partials 
+  and way we can pass local variables to a partial.
+3)what are delegates in rails and explain the usage of delegates?
+4)what do u mean by helper classes and explain.
+5)what do u mean by mailers?how many ways to pass arguments to a mailer?
+6)what do u mean by concerns in rails and way of using a concern
+7)have u ever used sidekiq and explain how it works both in production 
+  and development environments?
+8)what is difference between a sidekiq worker and rails activejob?
+9)what do mean by rale tasks ? how can u define/implement custom rake tasks?
+10)what do u know about ActiveStorage? How can u use this?
+=========================================================
+1)Explain strong parameters.
+2)How do u pass default arguments to a controller where u can instantiate a new model object?
+3)What are turbolinks into rails?
+4)what do u mean by API versioning in rails? 
+  Explain when there is a need to change the version of an API?
+5)before_save vs before_create vs before_update
+6)what do u mean by asset pipeline?
+7)Explain what is default_scope.
+8)Explain routing in rails.
+9)what are filter actions and controller callbacks. Explain
+10)How can u handle ActiveRecord::RecordNotFound exception fora all resources?
+===============================================================
+1)How many gems for the admin backend have u used? Which gem is better and why?
+2)If u are asked to implement an admin backend for ur web application what solution will you propose?
+  Any  gem or custom admin panel and also explain reason?
+3)save vs save!
+4)How can u pass params to a mailer action?
+5)Is it possible to replace schema.rb file in rails? If yes how?
+6)Explain ActionCable.
+7)How can u implement user authentication using device and JWT? Explain the process of it.
+8)How many formhelpers are provided by rails? Tell names and differences btween them.
+9)delete vs destroy in rails
+10)How to organize controllers? How to make a controller thin?
+==============================================================
+1)What do u mean by sessions in rails?Explain in what circumstances do u need to use sessions?
+2)What is find_each method in rails? Also explain why the .all method is not ideak to use for loading all the instances
+  of a model at once.
+3)What is polymorphic association in rails and when should we actually ise it?
+4)eager_load vs includes. which is better.
+5)Which is faster among delete and destroy. Explain.
+6)ActiveModel vs ActiveRecord
+7)dependent: :destroy vs dependent: :delete_all
+8)What is yield in ruby? What happens if u call a method, which includes yield, without passing a block?
+9)Hash vs HashWithIndifferentAccess
+10)How many ways to invoke a method with examples
+===========================================================
+1)String vs Symbols
+2)
