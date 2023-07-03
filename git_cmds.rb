7b5217cc90f18866f4cfea7456c9e847ba3a4824
askumardev <askumar.dev@outlook.com>
2023-07-03 17:55:24 +0530

    git cmds updated

--- git_cmds.rb
+++ git_cmds.rb
@@ -13,6 +13,17 @@
 * cat id_rsa.pub
 * Copy the above token an place in github account under Settings/SSH and GPG keys
 
+First commit commands
+Create a repo without selecting Readme.md
+Fill details like repo name and description
+echo "# exApp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/askumardev/exApp.git
+git push -u origin main
+
 If authentication error comes use ssh instead of https in git
 * git remote -v
 * git remote set-url origin git@github.com:ask***rdev/****.git
