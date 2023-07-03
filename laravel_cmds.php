3a6d1477ea09a3281b65448027f0993b47cd0a4f
askumardev <askumar.dev@outlook.com>
2023-07-03 16:58:02 +0530

    laravel tinker cmds

--- laravel_cmds.php
+++ laravel_cmds.php
@@ -1,1 +1,29 @@
 php artisan tinker
+
+> $user = new User;
+[!] Aliasing 'User' to 'App\Models\User' for this Tinker session.
+= App\Models\User {#6222}
+
+> $user->name = 'satish'
+= "satish"
+
+> $user->password = bcrypt('!password');
+= "$2y$10$3j..tCecWLh1XJ2bYtyWbOoovOAXn6yvT7qVH5FwjXOH93i1xAHeO"
+
+> $user->save();
+
+> $user->email = 'ask@ask.com';
+= "ask@ask.com"
+
+> $user->save();
+= true
+
+> $user
+App\Models\User {#6222
+    name: "satish",
+    #password: "$2y$10$3j..tCecWLh1XJ2bYtyWbOoovOAXn6yvT7qVH5FwjXOH93i1xAHeO",
+    updated_at: "2023-07-03 11:25:25",
+    created_at: "2023-07-03 11:25:25",
+    email: "ask@ask.com",
+    id: 2,
+  }
