790ab3626054e399d754abecbaa3593551457840
askumardev <askumar.dev@outlook.com>
2024-07-08 12:38:58 +0530

    emmet

---
+++ plugin_stuff.rb
@@ -0,0 +1,16 @@
+Emmet
+Select file type as html
+html:5 + hit tab button
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    .container>.row>table.table>(thead>th*2)+(tbody>(tr>td*3)
+    .container>.row>table.table>(thead>th*2)+(tbody>(tr>td*3)*3)
+  </body>
+</html>
